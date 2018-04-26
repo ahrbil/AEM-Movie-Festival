f46c871dba3987f2199b06e5bfb77a44a5c1f7ff
ahrbil <ahrbil.me@gmail.com>
2018-04-26 21:09:41 +0100

    movie componenet

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -3,18 +3,17 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-import './Movie.css';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w185';
 
 const Movie = ({ movie }) => (
-  <div className="Movie-card">
-    <Link to={`${movie.id}`}> 
+  <MovieCard>
+    <Link to={`${movie.id}`}>
       <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} draggable="false" />
       <p>{movie.release_date.split('-')[0]}</p>
-      <h3 className="Movie-title">{movie.title}</h3>
+      <h3>{movie.title}</h3>
     </Link>
-  </div>
+  </MovieCard>
 );
 
 export default Movie;
@@ -27,4 +26,23 @@ Movie.propTypes = {
 export const Poster = styled.img`
   box-shadow: 0 10px 40px rgba(0, 0, 0, 0.1);
   border-radius: 6px;
+`;
+
+const MovieCard = styled.div`
+  h3 {
+    font-size: 16px;
+    color: #000;
+    line-height: 20px;
+    width: 185px;
+    margin: 0 auto;
+  }
+  p {
+    color: rgba(141, 141, 141, 0.705);
+  }
+  img {
+    margin-bottom: 18px;
+  }
+  a {
+    text-decoration: none;
+  }
 `;
